b869efeeca555e1b32f6793818d3ceb3d80cbf49
unknown <you@example.com>
2017-12-13 12:03:34 +0100

    Gestion des timers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,28 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  
-  
   title = 'app';
   letableauX: Array<any> = [{ name: 'sdfsdf' }, { name: 'zerzerzsdf' }];
   letableau: String[];
-  
+  timerToken: any;
+
   constructor() {
     // attention cycle de vie de la class et du component 
     this.populateLetabelau();
     console.log("creation de la class AppComponent");
   }
-  
+
   private populateLetabelau(): void {
-    this.letableau = ['sdf', "sdf2"];
+    this.letableau = ['Test', "Test2"];
   }
-  
+
   laFonctionClickBt() {
     console.log("hehe le bnt est click");
+
+    // Le bouton relance le timer
+    this.ngOnInit();
   }
-  
+
   ngOnInit(): void {
-    console.log("ngOnInit de la class AppComponent");
-    // throw new Error("Method not implemented.");
+    console.log("ngOnInit valeur du token = " + this.timerToken);
+    
+    this.timerToken = setInterval(() => {
+      console.log("setInterval valeur du token = " + this.timerToken);
+      this.letableau.push(new Date().toISOString());
+    }, 1000);
+  }
+
+  stopTimer(): void {
+    console.log("stopTimer valeur du token = " + this.timerToken);
+    if (this.timerToken) {
+      clearTimeout(this.timerToken);
+      this.timerToken = null;
+    }
   }
 }
